b873c0384a31c6edd9e262d7738053c2ad7f787d
abhishekchandra2522k <1ac23456789@gmail.com>
2020-04-03 17:37:04 +0530

    Create Deterministic Finite Automata

---
+++ DFA.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    char str[100];
+    cin>>str;
+    int length = strlen(str);
+    if(str[0] == 'a' && str[length-1] == 'a')
+    {
+        cout<<"ACCEPTED";
+    }
+    else
+    {
+        cout<<"NOT ACCEPTED";
+    }
+return 0;
+}
