aa94be3d7e6f0ad45325f5facabfad57659db90e
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-16 22:14:28 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class Complex{
+int a,b;
+
+public:
+    Complex()
+    {
+        cout<<"Hi ";
+    }
+};
+
+
+int main()
+{
+    Complex a,b;
+    return 0;
+}
