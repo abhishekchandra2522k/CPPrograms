07d1b0dab8102fdb0b358f56e26939ff8905103d
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-17 22:14:43 +0530

    Add files via upload

---
+++ function_overloading.cpp
@@ -0,0 +1,44 @@
+/*Function Overloading*/
+#define pi 3.14
+#include<iostream>
+
+using namespace std;
+
+int area(int,int);
+
+float area(float);
+
+int main()
+{
+    int i,l,b;
+float r;
+    cout<<"1. Area of Circle\n2. Area of Rectangle\n";
+    cin>>i;
+    if(i==1)
+    {
+        cout<<"Enter radius of circle : ";
+        cin>>r;
+        cout<<"Area of Circle : "<<area(r);
+    }
+    if(i==2)
+    {
+        cout<<"Enter length : ";
+        cin>>l;
+        cout<<"Enter breadth : ";
+        cin>>b;
+        cout<<"Area of rectangle : "<<area(l,b);
+
+    }
+    return 0;
+}
+
+
+int area(int L,int B)
+{
+    return(L*B);
+}
+
+float area(float R)
+{
+    return(pi*R*R);
+}
