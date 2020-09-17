66923df606b06fadf0b2f033f0ca57f9620160d4
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-17 22:12:07 +0530

    Add files via upload

---
+++ anagram.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int i,t,count=0,j;
+	char s1[10001],s2[10001];
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+	cin>>s1;
+	cin>>s2;
+	for(j=0;j<strlen(t;j++)
+	{
+		if(s1[i]==s2[j])
+		{
+			count=0;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	}
+	cout<<count;
+return 0;
+}
