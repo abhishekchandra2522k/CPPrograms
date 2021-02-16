bb10078a281c246bfcfbef9001b9f9f589c3cb0f
abhishekchandra2522k <1ac23456789@gmail.com>
2021-02-16 21:31:21 +0530

    Add map_stl.cpp

---
+++ Standard Template Library /map_stl.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main(){
+    // Initialising map stl
+    map <int, string> customer;
+
+    customer[100] = "Gajendra";
+    customer[123] = "Dilip";
+    customer[145] = "Aditya";
+    customer[171] = "Shahid";
+    customer[200] = "Rajesh";
+
+    // OR 
+    // at the time of declaration
+
+    map <int, string> customer2{
+       {100,"Gajendra"}, {123,"Dilip"}, {145,"Aditya"}, {171, "Shahid"}, {200,"Rajesh"} 
+    };
+
+    // customer2.insert(pair<int, string>(122,"Doge"))
+    customer2.insert({122, "DOge"});
+    // USE of ITERATOR
+    map <int, string>:: iterator p = customer2.begin();
+    while(p != customer2.end()){
+        cout<<p->second<<endl;
+        p++;
+    }
+
+    return 0;
+}
