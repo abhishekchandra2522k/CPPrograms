23da7b42b6ffd198718fe0dbef24cd026b0e1da4
abhishekchandra2522k <1ac23456789@gmail.com>
2021-02-24 21:36:13 +0530

    Modify strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -29,4 +29,29 @@ int main(){
     string str4;
     str4 = str2 + str1 + str3;
     cout<<str4;
+
+    string str5;
+    cout<<"Enter your name : ";
+    cin>>str5; // space is a delimiter in cin class.
+    cout<<str5;
+
+    string str6;
+    str6 = s1 + str2; // Mixed Operations (char array) + string class
+
+    // Use of member functions of string class
+
+    string str7;
+    str7.assign("Block");
+    str7.append("chain");
+    str7.insert(10,"A");        // (index, string)
+    str7.replace(2,2,"Doge");  // (index, length, string)
+    int i = str7.find("Doge");  // returns index in case of found, else returns -1 in case of not found.
+    cout<<i;     
+    // str7.rfind("Doge");  // returns index from first occurance seen from end of string.
+    str7.compare(str6); // compares and return 0 if both strings are same, else if strings are in dict order it returns -1, else 1 (reverse dict order).
+    char str[20];
+    strcpy(str,str7.c_str());    // c_str converts string into char array
+    int j = str7.size(); // returns size / length of string
+    str7.erase(); // erase string from variable
+    cout<<str7;
 }
