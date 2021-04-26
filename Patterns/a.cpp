939d38e0381db85c1d5fcd2e781dcb83c3303b0e
abhishekchandra2522k <1ac23456789@gmail.com>
2021-04-26 21:44:58 +0530

    Add Patterns/

---
+++ Patterns/a.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i, j;
+
+    for(i = 0; i < 5; ++i){
+        for(j = 0; j < 5; ++j){
+            if(i>=j){
+                cout<<"*"<<"\t";
+            }
+            else{
+                cout<<"\t";
+            }
+        }
+        cout<<"\n";
+    }
+}
+
+/*
+
+*
+*       *
+*       *       *
+*       *       *       *
+*       *       *       *       * 
+
+*/
