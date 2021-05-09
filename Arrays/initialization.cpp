71b0899500f497245098ef6d7a11c6b4658b0f09
abhishekchandra2522k <1ac23456789@gmail.com>
2021-05-09 17:17:35 +0530

    Add Arrays/

---
+++ Arrays/initialization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>>
+
+using namespace std;
+
+int main(){
+    // Compile time Initialization of arrays
+    int a[5] = {3,1,7,1,7};  // 20(4Bytes(size of int) * 5(size of array)) Bytes of Memory Allocated, (32 bits each)
+    char b[10] = {'d','o','g','e'};
+    float c[4] = {1.2,2.3,36.717,37.550};
+
+    //Run time initialization
+    int d[10];
+    for(int i = 0; i<10; ++i){
+        cin>>d[i]; // Input numbers in the terminal on run time
+    }
+    return 0;
+}
