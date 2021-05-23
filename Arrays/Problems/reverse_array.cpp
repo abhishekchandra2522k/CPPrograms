0cb710f0254fc687999956d03497b7f25edd80ff
abhishekchandra2522k <1ac23456789@gmail.com>
2021-05-23 15:18:43 +0530

    Add reverse_array.cpp

---
+++ Arrays/Problems/reverse_array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void rev(int* arr, int n){
+    for(int i = 0; i < n/2; ++i){
+        swap(arr[i],arr[n-i-1]);
+    }
+}
+
+int main(){
+    int a[50], size;
+    cout<<"Enter size : ";
+    cin>>size;
+
+    if(size <= 50 && size >= 0){
+        cout<<"Enter elements : ";
+        for(int i = 0; i < size; ++i){
+            cin>>a[i];
+        }
+        rev(a, size);
+        for(int i = 0; i < size; ++i){
+            cout<<a[i]<<" ";
+        }
+    }
+    return 0;
+}
