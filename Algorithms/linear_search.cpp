14a84be6c341dc9a9e4dc11c4b26f93e3caccd20
abhishekchandra2522k <1ac23456789@gmail.com>
2021-05-27 21:18:27 +0530

    Add Algorithms/

---
+++ Algorithms/linear_search.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// This algo only returns Found or Not Found, checkout linear_search2.cpp for index at which the element is found.
+bool linear_search(int* a, int n, int k){
+    bool flag = false;
+    for(int i = 0; i < n; i++){
+        if(a[i] == k){
+            flag = true;
+        }
+    }
+    return flag;
+}
+
+int main(){
+    int a[50], size;
+    cout<<"Enter size : ";
+    cin>>size;
+    if(size <= 50 && size >= 0){
+        cout<<"Enter elements : ";
+        for(int i = 0; i < size; i++){
+            cin>>a[i];
+        }
+        int k;
+        cout<<"Enter element to find (this works on linear search algorithm for arrays in c++): ";
+        cin>>k;
+        if(linear_search(a,size,k)){
+            cout<<"Found"<<endl;
+        }else{
+            cout<<"Not Found"<<endl;
+        }
+    }else{
+        cout<<"Range error: size out of bound"<<endl;
+    }
+    return 0;
+}
---
+++ Algorithms/linear_search2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int linear_search(int* a, int n, int k){
+    int result = -1;
+    for(int i = 0; i < n; i++){
+        if(a[i] == k){
+            result = i; 
+            break;
+        }
+    }
+    return result;  // returns the index of first occurance of the element, removing break will give last occurance of the element
+}
+
+int main(){
+    int a[50], size;
+    cout<<"Enter size : ";
+    cin>>size;
+    if(size <= 50 && size >= 0){
+        cout<<"Enter elements : ";
+        for(int i = 0; i < size; i++){
+            cin>>a[i];
+        }
+        int k;
+        cout<<"Enter element to search : ";
+        cin>>k;
+        int res = linear_search(a,size,k);
+        if(res != -1){
+            cout<<"Position : "<<res+1<<", Index : "<<res<<endl;
+        }else{
+            cout<<"Not found"<<endl;
+        }
+    }else{
+        cout<<"Range error : size out of bound";
+    }
+    return 0;
+}
