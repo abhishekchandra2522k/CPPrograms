8bde2a7027a8588f3ddce24f8c4ad7b4a086e604
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-04 22:16:15 +0530

    Add cyclic_rotate_array_by_one.cpp

---
+++ Arrays/Problems/cyclic_rotate_array_by_one.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a[50],size;
+    cout<<"Enter size : ";
+    cin>>size;
+    if(size <= 50 && size >= 0){
+        cout<<"Enter elements : ";
+        for(int i = 0; i < size; i++){
+            cin>>a[i];
+        }
+        
+    }
+}
