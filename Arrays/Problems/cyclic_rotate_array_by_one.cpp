de3a99df13691211bba515d7d270a080826e6981
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-05 22:26:10 +0530

    Add cyclic_rotate_array_by_one.cpp

--- Arrays/Problems/cyclic_rotate_array_by_one.cpp
+++ Arrays/Problems/cyclic_rotate_array_by_one.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+void cyclic_rotate(int *a, int n){
+    int temp = a[n-1];
+    for(int i = n-2; i >= 0; i--){
+        a[i+1] = a[i];
+    }
+    a[0] = temp;
+}
+
 int main(){
     int a[50],size;
     cout<<"Enter size : ";
@@ -11,6 +19,12 @@ int main(){
         for(int i = 0; i < size; i++){
             cin>>a[i];
         }
-        
+        cyclic_rotate(a,size);
+        for(int i = 0; i < size; i++){
+            cout<<a[i]<<" ";
+        }
+    }else{
+        cout<<"Range error: index out of bound.";
     }
+    return 0;
 }
