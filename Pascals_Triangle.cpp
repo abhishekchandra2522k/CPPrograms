24ec6ef1ceca372a7aa4215f167cb0e8ee1f253b
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-09 19:40:26 +0530

    Add Pascals_Triangle.cpp

---
+++ Pascals_Triangle.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int fact(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+int main(){
+    int k;
+    cin>>k;
+    for(int i = 0; i < k; i++){
+        for(int  j = 0; j <= i; j++){
+            cout<<(fact(i)/(fact(j)*fact(i-j)));
+        }
+        cout<<endl;
+    }
+    return 0;
+}
