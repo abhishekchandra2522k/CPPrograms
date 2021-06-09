6fbf76c5b5c329a282822831462e653638ff5a4f
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-09 12:26:45 +0530

    Add CSES/

---
+++ CSES/weird_algorithms.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ll n;
+    cin>>n;
+    cout<<n;
+    while(n > 1){
+        if(n % 2 == 0){
+            n /= 2;
+        }else{
+            n = 3*n + 1;
+        }
+        cout<<" "<<n;
+    }
+    return 0;
+}
