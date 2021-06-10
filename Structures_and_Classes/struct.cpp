934a7ef0c122c0929e4ed706dd4fe42cfd3f344c
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-10 12:41:32 +0530

    Add Structures_and_Classes/

---
+++ Structures_and_Classes/struct.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+struct Student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
