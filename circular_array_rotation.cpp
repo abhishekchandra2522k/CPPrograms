f9a399bd13b457256d3fead243c3128f6296aa88
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-14 21:23:08 +0530

    Add circular_array_rotation.cpp

---
+++ circular_array_rotation.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'circularArrayRotation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY queries
+ */
+// Partially Accepted Solution
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
+    vector<int> res;
+    unsigned int s = a.size();
+    for(int i = 0; i < k; ++i){
+        int temp = a[s-1]; 
+        for(int j = s - 1; j >= 0; j--){
+            a[j+1] = a[j];
+        }
+        a[0] = temp; 
+    }
+    for(int i = 0; i < queries.size(); i++){
+        res.push_back(a[queries[i]]);
+    }
+    return res;
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    int q = stoi(first_multiple_input[2]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    vector<int> queries(q);
+
+    for (int i = 0; i < q; i++) {
+        string queries_item_temp;
+        getline(cin, queries_item_temp);
+
+        int queries_item = stoi(ltrim(rtrim(queries_item_temp)));
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> result = circularArrayRotation(a, k, queries);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
