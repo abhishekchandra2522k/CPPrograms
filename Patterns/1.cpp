17a937b6127dfa3573e7f8b3487398fa7a1dd882
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-04 21:57:48 +0530

    Update Patterns/

--- Patterns/1.cpp
+++ Patterns/1.cpp
@@ -20,6 +20,7 @@ int main(){
 }
 
 /*
+OUTPUT:
 
 *
 *       *
---
+++ Patterns/3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n; 
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(j < i){
+                cout<<" \t";
+            }else{
+                cout<<"*\t";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+/*
+OUTPUT:
+5
+*       *       *       *       *
+        *       *       *       *
+                *       *       *
+                        *       *
+                                *
+*/
