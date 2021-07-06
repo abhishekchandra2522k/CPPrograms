40b663156a1a9c3c50ff9923da3a206d1f2f5fa9
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-06 22:14:45 +0530

    Add sequence_equation.cpp

---
+++ sequence_equation.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'permutationEquation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+vector<int> permutationEquation(vector<int> p) {
+    vector<int> res;
+    for(int x = 1; x <= p.size(); x++){
+        auto it = find(p.begin(), p.end(), x);
+        int i = it - p.begin() + 1;
+        auto it2 = find(p.begin(), p.end(), i);
+        int j = it2 - p.begin() + 1;
+        res.push_back(j);
+    }
+    
+    return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp_temp;
+    getline(cin, p_temp_temp);
+
+    vector<string> p_temp = split(rtrim(p_temp_temp));
+
+    vector<int> p(n);
+
+    for (int i = 0; i < n; i++) {
+        int p_item = stoi(p_temp[i]);
+
+        p[i] = p_item;
+    }
+
+    vector<int> result = permutationEquation(p);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
