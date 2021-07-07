538fed8ce6ca65dcd62318d36ff497d6f286c864
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-07 22:06:04 +0530

    Add ListLength function in singly_linked_list.cpp

--- Linked List/singly_linked_list.cpp
+++ Linked List/singly_linked_list.cpp
@@ -2,14 +2,29 @@
 
 using namespace std;
 
-struct node{
+// Defining ListNode LinkedList (User Defined Data Type)
+struct ListNode{
     int data;
-    struct node *next;
+    struct ListNode *next;
 };
 
-struct node *head, *newnode;
+// Defining its operations (Abstract Data Type)
+
+// ListLength function takes a head pointer as a parameter to calculate length of a linked list
+int ListLength(struct ListNode *head){
+    struct ListNode *current = head;
+    int count = 0;
+
+    while(current != NULL){
+        count++;
+        current = current->next;
+    }
+    return count;
+};
+
 
 int main(){
-    struct node first;
+    struct ListNode firstLL;
+    cout<<ListLength(firstLL.next);
     return 0;
 }
