fc7b00b9b8245dbc917e27ab9d550d6e3a01a5f7
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-08 22:00:22 +0530

    Add InsertInLinkedList function in singly_linked_list.cpp

--- Linked List/singly_linked_list.cpp
+++ Linked List/singly_linked_list.cpp
@@ -22,9 +22,40 @@ int ListLength(struct ListNode *head){
     return count;
 };
 
+// Singly Linked List Insertion 
+// Insertion can be performed in three ways
+void InsertInLinkedList(struct ListNode **head, int data, int position){
+    int k = 1;
+    struct ListNode *p, *q, *newNode;
+    newNode = (ListNode *)malloc(sizeof(struct ListNode));
+    if(!newNode){
+        cout<<"Memory Error";
+        return;
+    }
+    newNode->data = data;
+    p = *head;
+
+    // Inserting at the beginning
+    if(position == 1){
+        newNode->next = p;
+        *head = newNode;
+    }else{
+        // Traverse the list until the position where we want to insert
+        while((p != NULL) && (k < position)){
+            k++;
+            q = p;
+            p = p->next;
+        }
+        q->next = newNode; //more optimum way to do this
+        newNode->next = p;
+    }
+}
+
 
 int main(){
-    struct ListNode firstLL;
+    struct ListNode firstLL, **head = NULL;
+    InsertInLinkedList(NULL, 25, 1);
     cout<<ListLength(firstLL.next);
+
     return 0;
 }
