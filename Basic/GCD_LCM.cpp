f5deb7fc495a3e98deb3eed8e8adea4db74f80ae
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-16 11:01:20 +0530

    Add Basic/

---
+++ Basic/GCD_LCM.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int gcd(int, int);
+int lcm(int, int);
+
+int gcd(int a, int b){
+    while(a && b){
+        if(a > b){
+            a %= b;
+        }else{
+            b %= a;
+        }
+    }
+    return a+b;
+}
+
+int lcm(int a, int b){
+    return (a / gcd(a, b)) * b;
+}
+
+int main(){
+    int x,y;
+    cout<<"Enter two numbers : ";
+    cin>>x>>y;
+    cout<<"GCD / HCF: "<<gcd(x, y)<<" LCM: "<<lcm(x,y)<<endl;
+    return 0;
+}
