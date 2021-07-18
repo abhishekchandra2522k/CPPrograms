b7dc8f85e07b79f8bf4ea1c8ff0f7714f2a8c07c
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-18 20:56:33 +0530

    Modify sorting_in_one_loop.cpp

--- Algorithms/sorting/sorting_in_one_loop.cpp
+++ Algorithms/sorting/sorting_in_one_loop.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int arr[10] = {0, 8, 1, 1, 5, 2, 3, 2, 1, -1};
     for (int i = 0; i < 9; i++)
     {
         if (arr[i] > arr[i + 1])
