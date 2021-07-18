148808815dc09674fd7bb60fc2b65978f7ff2f80
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-18 20:56:52 +0530

    Add game_with_numbers.cpp

---
+++ GFG/game_with_numbers.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int *game_with_numbers(int arr[], int n);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        int *arr2;
+
+        arr2 = game_with_numbers(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr2[i] << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int *game_with_numbers(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = arr[i] ^ arr[i + 1];
+    }
+    return arr;
+}
