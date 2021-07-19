d2086576922b93f83842aef3c8d736de7afe3992
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-19 13:12:47 +0530

    Mpdify singly_linked_list_3.cpp

--- Linked List/singly_linked_list_3.cpp
+++ Linked List/singly_linked_list_3.cpp
@@ -1,63 +1,37 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-class node{
-    private:
-        int data;
-        node *next;
+class Node{
     public:
-        node *head;
-        int listLength(node *head){
-            node* temp = head;
-            int count = 0;
-            while(temp != NULL){
-                count++;
-                temp = temp->next;
-            }
-            return count;
-        }
-        void createNode(node **head, int data, int position){
-            int k = 1;
-            node *p,*q, *newNode;
-            newNode = (node*)malloc(sizeof(node));
-
-        }
+    int data;
+    Node* next;
 };
 
-int menu(){
-    int choice;
-    cout<<"1. Create Node."<<endl;
-    cout<<"2. Delete Node."<<endl;
-    cout<<"3. List Length."<<endl;
-    cout<<"4. View List."<<endl;
-    cout<<"5. Exit"<<endl;
-    cout<<"Select Choice : ";
-    cin>>choice;
-    return choice;
+void printList(Node* temp){
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
 }
 
 int main(){
-    node firstLL;
-    firstLL.head = NULL;
-    while(true){
-        int choice = menu();
-        if(choice == 1){
-            int data, pos;
-            cout<<"Enter data : ";
-            cin>>data;
-            cout<<"Enter Position : ";
-            cin>>pos;
-            firstLL.createNode(&head, data, pos);
-        }else if(choice == 2){
-            int pos;
-            cout<<"Enter position : ";
-            cin>>pos;
-            deleteNode(&head, pos);
-        }else if(choice == 3){
-            cout<<firstLL.listLength(firstLL.head);
-        }
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
 
-    }
-    return 0;
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    printList(head);
 }
