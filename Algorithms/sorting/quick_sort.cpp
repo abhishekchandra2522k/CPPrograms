27dd349e84dcb72ed4060fbfb44587e1bf36ec42
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-22 12:51:38 +0530

    Add quick_sort.cpp

---
+++ Algorithms/sorting/quick_sort.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void quickSort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    int partition(int arr[], int low, int high)
+    {
+        int i = low - 1;
+        int pivot = arr[high];
+
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        return i + 1;
+    }
+};
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        ob.quickSort(arr, 0, n - 1);
+        printArray(arr, n);
+    }
+    return 0;
+}
