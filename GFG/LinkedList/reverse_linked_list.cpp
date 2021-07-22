7154b5f8de99e9cbc0baae91a96f25e4754206ad
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-22 13:09:20 +0530

    Add reverse_linked_list.cpp

---
+++ GFG/LinkedList/reverse_linked_list.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    struct Node *reverseList(struct Node *head)
+    {
+        Node *current = head, *prev = NULL, *next;
+        while (current != NULL)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+};
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int t, n, l, firstdata;
+    cin >> t;
+    while (t--)
+    {
+        Node *head = NULL, *tail = NULL;
+
+        cin >> n;
+
+        cin >> firstdata;
+        head = new Node(firstdata);
+        tail = head;
+
+        for (int i = 1; i < n; i++)
+        {
+            cin >> l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+        Solution ob;
+
+        head = ob.reverseList(head);
+
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
