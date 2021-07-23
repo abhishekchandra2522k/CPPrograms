b112e5199b2aee845e288533ecdc69bdaec92e41
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-23 17:04:58 +0530

    Add list_of_prime_till_n.cpp

--- Basic/list_of_prime_till_n.cpp
+++ Basic/list_of_prime_till_n.cpp
@@ -1,13 +1,30 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    for(int i = 0; i < n; i++){
-        for(int j = 2; j < n; j++){
-            
+int main()
+{
+    int n, count, c, i = 3;
+    cin >> n;
+
+    if (n >= 1)
+    {
+        cout << 2 << " ";
+    }
+
+    for (count = 2; count <= n; i++)
+    {
+        for (c = 2; c < i; c++)
+        {
+            if (i % c == 0)
+                break;
+        }
+
+        if (c == i)
+        {
+            cout << i << " ";
+            count++;
         }
     }
+    return 0;
 }
