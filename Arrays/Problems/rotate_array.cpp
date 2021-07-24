acd5066a94ebe60af1e8bdcdeabe4deb700327b9
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-24 20:05:13 +0530

    Update rotate_array.cpp solution

--- Arrays/Problems/rotate_array.cpp
+++ Arrays/Problems/rotate_array.cpp
@@ -13,6 +13,11 @@ public:
 
     void leftRotate(int arr[], int n, int d)
     {
+        if (d == 0)
+        {
+            return;
+        }
+        d %= n;
         reverse(arr, 0, n - 1);
         reverse(arr, 0, n - d - 1);
         reverse(arr, n - d, n - 1);
