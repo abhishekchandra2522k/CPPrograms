df780503f28e03e9f390b22785147651e9bc3bb9
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-24 20:03:56 +0530

    Add rotate_array.cpp

---
+++ Arrays/Problems/rotate_array.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    void leftRotate(int arr[], int n, int d)
+    {
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, n - d - 1);
+        reverse(arr, n - d, n - 1);
+    }
+
+    void reverse(int arr[], int l, int e)
+    {
+        while (l < e)
+        {
+            swap(&arr[l], &arr[e]);
+            l++;
+            e--;
+        }
+    }
+};
+
+int main()
+{
+    //code
+    int t, n, d;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> d;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
