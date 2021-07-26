743ec3b94217e59869ce304e9f4cb74a67a5133a
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-26 20:35:14 +0530

    Add lower_bound_STL.cpp

---
+++ hackerrank/lower_bound_STL.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int q, val;
+    cin >> q;
+    for (int i = 0; i < q; i++)
+    {
+        cin >> val;
+        vector<int>::iterator low = lower_bound(arr.begin(), arr.end(), val);
+
+        if (arr[low - arr.begin()] == val)
+        {
+            cout << "Yes " << (low - arr.begin() + 1) << endl;
+        }
+        else
+        {
+            cout << "No " << (low - arr.begin() + 1) << endl;
+        }
+    }
+
+    return 0;
+}
