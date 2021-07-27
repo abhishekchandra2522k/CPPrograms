eabca38d7243a8aec6252f522fa788dda2ef5c18
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-27 11:28:59 +0530

    Add execute_both_if_else_simultaneously.cpp

---
+++ Basic/exectute_both_if_else_simultaneously.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    if (0)
+    {
+    label_1:
+        std::cout << "Hello";
+        goto label_2;
+    }
+    else
+    {
+        goto label_1;
+
+    label_2:
+        std::cout << " Geeks!";
+    }
+    return 0;
+}
