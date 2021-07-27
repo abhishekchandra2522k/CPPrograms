2b5d3e4422fe046c1dd80cd5677ce3b2d9dcd3af
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-27 13:05:52 +0530

    Add reverse_string_using_constructor.cpp

---
+++ Strings/reverse_string_using_constructor.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s = "gnirts desrever a si sihT";
+    string rev = string(s.rbegin(), s.rend());
+    cout << rev << endl;
+    return 0;
+}
