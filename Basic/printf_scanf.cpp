5fc3856b3dac1786c956aa9ef6d271b3c9eac8b4
abhishekchandra2522k <1ac23456789@gmail.com>
2021-08-01 19:08:26 +0530

    Add printf_scanf.cpp

---
+++ Basic/printf_scanf.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    printf("%d\n", printf("%d\n", a));
+
+    printf("%d\n", printf("%d\n", 1234));
+
+    printf("%d\n", printf("Abhishek\n"));
+
+    return 0;
+}
