4268a1ba71313b869b0e856c7292845bd1912072
abhishekchandra2522k <1ac23456789@gmail.com>
2021-08-05 12:05:54 +0530

    Add volume_of_sphere.cpp

---
+++ Basic/volume_of_sphere.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double radius;
+
+    cin >> radius;
+
+    cout << (4.0 / 3) * (22.0 / 7) * (radius) * (radius) * (radius);
+    return 0;
+}
