47c50e2e39eac22d64c1eeda5fcce7161ddd1b95
abhishekchandra2522k <1ac23456789@gmail.com>
2021-08-09 15:37:40 +0530

    Add repeated_strings.cpp

---
+++ hackerrank/repeated_strings.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+long repeatedString(string s, long n) {
+    long ans = 0;
+    if(s == "a"){
+        return n;
+    }
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == 'a'){
+            ans++;
+        }    
+    }
+    long m = n / s.size();
+    long r = n % s.size();
+    ans *= m;
+    
+    for(int i = 0; i < r; i++){
+        if(s[i] == 'a'){
+            ans++;
+        }
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    long result = repeatedString(s, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
