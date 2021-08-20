71fab411168789365e193da221fb914bc2216d4b
abhishekchandra2522k <1ac23456789@gmail.com>
2021-08-20 21:57:43 +0530

    Update Pascals_Triangle.cpp

--- hackerrank/Pascals_Triangle.cpp
+++ hackerrank/Pascals_Triangle.cpp
@@ -1,22 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int fact(int n){
-    if(n == 0){
+int fact(int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
-    return n*fact(n-1);
+    return n * fact(n - 1);
 }
 
-int main(){
+int main()
+{
     int k;
-    cin>>k;
-    for(int i = 0; i < k; i++){
-        for(int  j = 0; j <= i; j++){
-            cout<<(fact(i)/(fact(j)*fact(i-j)));
+    cin >> k;
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << (fact(i) / (fact(j) * fact(i - j))) << "\t";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
